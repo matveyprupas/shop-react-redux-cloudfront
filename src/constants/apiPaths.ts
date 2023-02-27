10e2e363a1e9afdbba62c2f349215e29a0f2cae5
Matvey Prupas <matvey_prupas@epam.com>
2023-02-27 23:10:41 +0200

    fix: fix FE part according with BE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
