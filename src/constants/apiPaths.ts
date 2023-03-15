c580d3f42794a8d68aa6a4e85104ece726ada9bd
Matvey Prupas <matvey_prupas@epam.com>
2023-03-15 12:29:17 +0200

    feat: integrate import files with FE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://gn2rfzlo13.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://da1uu0as0a.execute-api.eu-west-1.amazonaws.com/dev",
 };
